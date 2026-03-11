6869bb90ca66d56a1d0ec4526fb8605aa3d247ff
Miguel Ramirez <miguel.matos.fernandes1@gmail.com>
2026-03-11 12:56:16 +0000

    Fix leaks in parent process

--- philo_bonus/src/monitor.c
+++ philo_bonus/src/monitor.c
@@ -6,7 +6,7 @@
 /*   By: migusant <migusant@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/17 15:31:40 by migusant          #+#    #+#             */
-/*   Updated: 2026/03/11 11:42:14 by migusant         ###   ########.fr       */
+/*   Updated: 2026/03/11 12:54:32 by migusant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,16 +41,20 @@ void	*monitor_routine(void *arg)
 		time_since_meal = get_time_ms() - philo->last_meal_time;
 		if (time_since_meal >= philo->data->time_to_die)
 		{
+			if (is_simulation_stopped(philo->data))
+				return (NULL);
 			sem_wait(philo->data->print_sem);
-			if (!is_simulation_stopped(philo->data))
+			if (is_simulation_stopped(philo->data))
 			{
-				stop_simulation(philo->data);
-				printf("%ld %d died\n",
-					get_elapsed_time(philo->data->start_time),
-					philo->id);
-				if (PHILO_DEBUG)
-					print_meal_summary("Failed");
+				sem_post(philo->data->print_sem);
+				return (NULL);
 			}
+			stop_simulation(philo->data);
+			printf("%ld %d died\n",
+				get_elapsed_time(philo->data->start_time),
+				philo->id);
+			if (PHILO_DEBUG)
+				print_meal_summary("Failed");
 			sem_post(philo->data->print_sem);
 			exit(1);
 		}
