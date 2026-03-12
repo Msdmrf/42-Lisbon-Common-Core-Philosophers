b25e035217618e46187bb352bb1f613f7279346b
Miguel Ramirez <miguel.matos.fernandes1@gmail.com>
2026-03-12 22:46:53 +0000

    Merge signal handling in mandatory part

--- philo/src/signals.c
+++ philo/src/signals.c
@@ -6,7 +6,7 @@
 /*   By: migusant <migusant@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/09 09:18:48 by migusant          #+#    #+#             */
-/*   Updated: 2026/03/08 17:06:30 by migusant         ###   ########.fr       */
+/*   Updated: 2026/03/12 22:37:18 by migusant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ t_singleton	*singleton(void)
 	return (&instance);
 }
 
-static void	handle_sigint(int sig)
+static void	handle_interrupt(int sig)
 {
 	(void)sig;
 	if (singleton()->data)
@@ -27,18 +27,6 @@ static void	handle_sigint(int sig)
 		stop_simulation();
 		if (PHILO_DEBUG)
 			print_meal_summary("Interrupted");
-	}
-}
-
-static void	handle_sigquit(int sig)
-{
-	(void)sig;
-	if (singleton()->data)
-	{
-		stop_simulation();
-		if (PHILO_DEBUG)
-			print_meal_summary("Interrupted");
-		write(STDOUT_FILENO, "Quit\n", 5);
 	}
 }
 
@@ -46,12 +34,14 @@ void	setup_signals(int mode)
 {
 	if (mode == SIG_SETUP)
 	{
-		signal(SIGINT, handle_sigint);
-		signal(SIGQUIT, handle_sigquit);
+		signal(SIGINT, handle_interrupt);
+		signal(SIGQUIT, handle_interrupt);
+		signal(SIGTERM, handle_interrupt);
 	}
 	else if (mode == SIG_RESET)
 	{
 		signal(SIGINT, SIG_DFL);
 		signal(SIGQUIT, SIG_DFL);
+		signal(SIGTERM, SIG_DFL);
 	}
 }
